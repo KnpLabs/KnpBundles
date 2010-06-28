6cebfd64cc24a12ee1773d03866d2442f0e07790
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-28 23:12:06 +0200

    Rename configuration folder to front

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../symfony2bundles/FrontKernel.php';
+require_once __DIR__.'/../front/FrontKernel.php';
 
 $kernel = new FrontKernel('prod', false);
 $kernel->handle()->send();
