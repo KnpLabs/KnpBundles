e4956ca2264d33a12030faef8194145416bd1620
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-07-09 18:18:48 +0200

    Add functional tests for homepage and API doc

---
+++ src/Application/S2bBundle/Tests/Controllers/MainControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Bundle\S2bBundle\Tests\Controller;
+
+use Symfony\Framework\FoundationBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $this->assertEquals(1, $crawler->filter('h1:contains("Symfony2 Bundles")')->count());
+        $this->assertEquals(1, $crawler->filter('input#qsearch')->count());
+    }
+
+    public function testApi()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/api');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $this->assertEquals(1, $crawler->filter('h1:contains("Developer API")')->count());
+        $this->assertEquals(1, $crawler->filter('h3:contains("List bundles")')->count());
+    }
+}
+
