5e849b911d508f6facf6833bf1e20c0a2628692e
ornicar <thibault.duplessis@gmail.com>
2010-07-30 10:10:23 +0200

    Ugly fix to Doctrine Proxies autoloading issue

--- s2b/S2bKernel.php
+++ s2b/S2bKernel.php
@@ -13,6 +13,21 @@ class S2bKernel extends Kernel
         return __DIR__;
     }
 
+    public function boot()
+    {
+        Symfony\Components\OutputEscaper\Escaper::markClassesAsSafe(array(
+            'Symfony\Components\Form\Form',
+            'Symfony\Components\Form\Field'
+        ));
+
+        #TODO remove me 
+        foreach(array('ApplicationS2bBundleEntityUserProxy') as $class) {
+            @include_once(__DIR__.'/cache/'.$this->getEnvironment().'/doctrine/orm/Proxies/'.$class.'.php');
+        }
+        
+        return parent::boot();
+    }
+
     public function registerBundles()
     {
         $bundles = array(
