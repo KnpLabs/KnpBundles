e61d5faf08442e23997d2b3b4d8fe92615d1c011
ornicar <thibault.duplessis@gmail.com>
2010-08-11 11:27:12 +0200

    Upgrade all vendors & TimeBundle to latest Symfony2 style

--- src/Bundle/TimeBundle/TimeBundle.php
+++ src/Bundle/TimeBundle/TimeBundle.php
@@ -2,15 +2,8 @@
 
 namespace Bundle\TimeBundle;
 
-use Bundle\TimeBundle\DependencyInjection\TimeExtension;
 use Symfony\Framework\Bundle\Bundle as BaseBundle;
-use Symfony\Components\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Components\DependencyInjection\ContainerBuilder;
 
 class TimeBundle extends BaseBundle
 {
-    public function buildContainer(ParameterBagInterface $parameterBag)
-    {
-        ContainerBuilder::registerExtension(new TimeExtension());
-    }
 }
