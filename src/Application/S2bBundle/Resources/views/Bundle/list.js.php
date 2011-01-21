87910d69a84a689906ff81e768ab3032e66cf148
Antoine Hérault <antoine.herault@gmail.com>
2011-01-21 19:29:32 +0100

    Replace php templates by twig for bundle controller

--- src/Application/S2bBundle/Resources/views/Bundle/list.js.php
+++
@@ -1,1 +0,0 @@
-<?php $view->output('S2bBundle:Repo:list', array('repos' => $repos, 'callback' => $callback)) ?>
--- src/Application/S2bBundle/Resources/views/Bundle/show.php
+++
@@ -1,6 +0,0 @@
-<?php $view->extend('S2bBundle:Repo:show') ?>
-
-<?php $view['slots']->set('h1', $repo->getShortName().'<span>Bundle</span>') ?>
-<?php $view['main_menu']['Bundles']->setIsCurrent(true) ?>
-
-<?php $view['slots']->set('git_command', sprintf('git submodule add %s src/Bundle/%s', $repo->getGitUrl(), $repo->getName())) ?>
