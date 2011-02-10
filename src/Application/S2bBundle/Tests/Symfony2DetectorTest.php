d1b22dc5960657e1dbc3ac347a7d85957714a723
ornicar <thibault.duplessis@gmail.com>
2011-02-09 19:26:26 -0800

    Add Symfony2Detector with unit tests

---
+++ src/Application/S2bBundle/Tests/Symfony2DetectorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Application\S2bBundle;
+
+use PHPUnit_Framework_TestCase;
+
+class RepoTest extends PHPUnit_Framework_TestCase
+{
+    public function testHasNoSubmodules()
+    {
+        $this->assertFalse($this->getDetector('repo1')->isProject());
+    }
+
+    public function testHasNoSymfonySubmodule()
+    {
+        $this->assertFalse($this->getDetector('repo2')->isProject());
+    }
+
+    public function testHasSymfonySubmodule()
+    {
+        $this->assertTrue($this->getDetector('repo3')->isProject());
+    }
+
+    public function testHasEmptyKernel()
+    {
+        $this->assertFalse($this->getDetector('repo4')->isProject());
+    }
+
+    public function testHasKernel()
+    {
+        $this->assertTrue($this->getDetector('repo5')->isProject());
+    }
+
+    public function testHasTwoKernels()
+    {
+        $this->assertTrue($this->getDetector('repo6')->isProject());
+    }
+
+    private function getDetector($fixtureName)
+    {
+        return new Symfony2Detector($this->getFixture($fixtureName));
+    }
+
+    private function getFixture($name)
+    {
+        return realpath(__DIR__.'/fixtures/'.$name);
+    }
+}
