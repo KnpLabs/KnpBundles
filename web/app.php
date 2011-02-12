5fdb78ca6a25451e2579b204aeba75370c95ad99
Matthieu Bontemps <matthieu@knplabs.com>
2011-02-12 10:25:06 +0100

    Disable debugging in prod

--- web/app.php
+++ web/app.php
@@ -5,5 +5,5 @@ require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('prod', false);
 $kernel->handle(Request::createFromGlobals())->send();
