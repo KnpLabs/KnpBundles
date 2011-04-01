8670469bd71878d7d9a4476ae8b37a0ea28394df
Antoine Hérault <antoine.herault@gmail.com>
2011-04-01 16:21:15 +0200

    Add migration file for the description field

---
+++ src/Knplabs/Symfony2BundlesBundle/DoctrineMigrations/Version20110401160547.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Knplabs\Symfony2BundlesBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20110401160547 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->_addSql('ALTER TABLE repo CHANGE description description VARCHAR(255) NULL DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->_addSql('ALTER TABLE repo CHANGE description description VARCHAR(255) NOT NULL');
+    }
+}
