4127b1cc8cf41831f5d75a7f9a3435e8b3108e8f
Antoine Hérault <antoine.herault@gmail.com>
2011-04-04 22:48:39 +0200

    Update Symfony2BundlesBundle to register the extension
    As the bundle name contains the "Bundle" word, the HttpKernel is not
    able to automatically register the extension. So we handle it manually.

--- src/Knplabs/Bundle/Symfony2BundlesBundle/KnplabsSymfony2BundlesBundle.php
+++ src/Knplabs/Bundle/Symfony2BundlesBundle/KnplabsSymfony2BundlesBundle.php
@@ -2,8 +2,20 @@
 
 namespace Knplabs\Bundle\Symfony2BundlesBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Knplabs\Bundle\Symfony2BundlesBundle\DependencyInjection\KnplabsSymfony2BundlesExtension;
 
 class KnplabsSymfony2BundlesBundle extends Bundle
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->registerExtension(new KnplabsSymfony2BundlesExtension());
+    }
+
 }
