67211d47c1e629df5ddb10eaaf779ec4cb891356
Christophe Coevoet <stof@notk.org>
2011-07-19 13:00:12 +0200

    Removed the Js functions from the global scope

--- src/Knp/Bundle/Symfony2BundlesBundle/Resources/public/js/keyboard.navigation.js
+++ src/Knp/Bundle/Symfony2BundlesBundle/Resources/public/js/keyboard.navigation.js
@@ -1,77 +1,79 @@
-//code to control keyboard navigation
-var curPosition = 0;
+(function($) {
+    //code to control keyboard navigation
+    var curPosition = 0;
 
-function onload() {
+    $(function() {
 
-    if ($('.repo').length > 0) {
-        var className = 'repo';
-    } else if ($('.developer').length > 0) {
-        var className = 'developer';
-    }
-
-    $('.' + className).first().addClass('active');
-
-    $(document).bind('keydown', 's', function() { $('#search-query').focus(); return false; });
-    $('#search-query').bind('keydown', 'esc', function() { $('#search-query').blur(); return false; });
-
-    $(document).bind('keydown', 'down', function() {
-        moveElement(className, 'down');
-
-        return false;
-    });
-
-    $(document).bind('keydown', 'j', function() {
-        moveElement(className, 'down');
-
-        return false;
-    });
-
-
-    $(document).bind('keydown', 'up', function(){
-        moveElement(className, 'up');
-
-        return false;
-    });
-    $(document).bind('keydown', 'k', function(){
-        moveElement(className, 'up');
-
-        return false;
-    });
-
-    $(document).bind('keydown', 'return', function() {
-        if (className == 'repo') {
-            document.location = $('.' + className).eq(curPosition).children('.generals').children('.repo-title').children('.name').attr('href');
-        } else if (className == 'developer') {
-            document.location = $('.' + className).eq(curPosition).children('.generals').children('.name').attr('href');
+        if ($('.repo').length > 0) {
+            var className = 'repo';
+        } else if ($('.developer').length > 0) {
+            var className = 'developer';
         }
 
-        return false;
-    });
-}
+        $('.' + className).first().addClass('active');
 
-function moveElement(className ,direction)
-{
-    var elem = $('.' + className);
-    var listCount = elem.size();
-    if (direction == 'down') {
-        if (++curPosition < listCount) {
-            console.log(curPosition);
-            elem.eq(curPosition-1).removeClass('active');
-            elem.eq(curPosition).addClass('active');
+        $(document).bind('keydown', 's', function() { $('#search-query').focus(); return false; });
+        $('#search-query').bind('keydown', 'esc', function() { $('#search-query').blur(); return false; });
+
+        $(document).bind('keydown', 'down', function() {
+            moveElement(className, 'down');
+
+            return false;
+        });
+
+        $(document).bind('keydown', 'j', function() {
+            moveElement(className, 'down');
+
+            return false;
+        });
+
+
+        $(document).bind('keydown', 'up', function(){
+            moveElement(className, 'up');
+
+            return false;
+        });
+        $(document).bind('keydown', 'k', function(){
+            moveElement(className, 'up');
+
+            return false;
+        });
+
+        $(document).bind('keydown', 'return', function() {
+            if (className == 'repo') {
+                document.location = $('.' + className).eq(curPosition).children('.generals').children('.repo-title').children('.name').attr('href');
+            } else if (className == 'developer') {
+                document.location = $('.' + className).eq(curPosition).children('.generals').children('.name').attr('href');
+            }
+
+            return false;
+        });
+    });
+
+    function moveElement(className ,direction)
+    {
+        var elem = $('.' + className);
+        var listCount = elem.size();
+        if (direction == 'down') {
+            if (++curPosition < listCount) {
+                console.log(curPosition);
+                elem.eq(curPosition-1).removeClass('active');
+                elem.eq(curPosition).addClass('active');
+            } else {
+                curPosition--;
+            }
         } else {
-            curPosition--;
+            if (--curPosition >= 0) {
+                elem.eq(curPosition+1).removeClass('active');
+                elem.eq(curPosition).addClass('active');
+            } else{
+                curPosition++;
+            }
         }
-    } else {
-        if (--curPosition >= 0) {
-            elem.eq(curPosition+1).removeClass('active');
-            elem.eq(curPosition).addClass('active');
-        } else{
-            curPosition++;
-        }
-    }
 
-    //check if the element is in view, if not move to it
-    if (elem.eq(curPosition).viewportOffset().top > $(window).height() || elem.eq(curPosition).viewportOffset().top < 0) {
-        $('html, body').animate({scrollTop: elem.eq(curPosition).offset().top}, 500);
+        //check if the element is in view, if not move to it
+        if (elem.eq(curPosition).viewportOffset().top > $(window).height() || elem.eq(curPosition).viewportOffset().top < 0) {
+            $('html, body').animate({scrollTop: elem.eq(curPosition).offset().top}, 500);
+        }
     }
-}
+})(jQuery)
