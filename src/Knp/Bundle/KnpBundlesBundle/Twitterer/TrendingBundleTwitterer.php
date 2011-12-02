42611f115beddcab2b80863ef2c1711a6d50ca46
Alexandr Torchenko <torchenko@gmail.com>
2011-12-02 22:26:53 +0200

    Indentation fix

--- src/Knp/Bundle/KnpBundlesBundle/Twitterer/TrendingBundleTwitterer.php
+++ src/Knp/Bundle/KnpBundlesBundle/Twitterer/TrendingBundleTwitterer.php
@@ -6,35 +6,35 @@ use Knp\Bundle\KnpBundlesBundle\Twitterer\Exception\TrendingBundleNotFound;
 
 class TrendingBundleTwitterer
 {
-	private $em;
-	private $tweetTemplate;
-	private $twitterService;
+    private $em;
+    private $tweetTemplate;
+    private $twitterService;
 
-	public function __construct($em, $tweetTemplate, $twitterService)
-	{
-		$this->em = $em;
-		$this->tweetTemplate = $tweetTemplate;
-		$this->twitterService = $twitterService;
-	}
+    public function __construct($em, $tweetTemplate, $twitterService)
+    {
+        $this->em = $em;
+        $this->tweetTemplate = $tweetTemplate;
+        $this->twitterService = $twitterService;
+    }
 
-	public function tweet()
-	{
-		$message = $this->prepareMessage();
-		$this->twitterService->tweet($message);
-	}
+    public function tweet()
+    {
+        $message = $this->prepareMessage();
+        $this->twitterService->tweet($message);
+    }
 
-	private function prepareMessage()
-	{
-		if (!$trendingBundle = $this->em->getRepository('KnpBundlesBundle:Bundle')->findLatestSortedBy('trend1')) {
-			throw new TrendingBundleNotFound();
-		}
+    private function prepareMessage()
+    {
+        if (!$trendingBundle = $this->em->getRepository('KnpBundlesBundle:Bundle')->findLatestSortedBy('trend1')) {
+            throw new TrendingBundleNotFound();
+        }
 
-		$bundleName = $trendingBundle->getName();
-		$url = 'bundles.knplabs.org/' . $trendingBundle->getUsername() . '/' . $bundleName;
+        $bundleName = $trendingBundle->getName();
+        $url = 'bundles.knplabs.org/' . $trendingBundle->getUsername() . '/' . $bundleName;
 
-		$placeholders = array('{name}', '{url}');
-		$values = array($bundleName, $url);
+        $placeholders = array('{name}', '{url}');
+        $values = array($bundleName, $url);
 
-		return str_replace($placeholders, $values, $this->tweetTemplate);
-	}
+        return str_replace($placeholders, $values, $this->tweetTemplate);
+    }
 }
