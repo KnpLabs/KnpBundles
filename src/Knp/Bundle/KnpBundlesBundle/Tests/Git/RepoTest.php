9b8849fcef658e8a54ecc1f52f3ca8b68bdca868
Joseph Bielawski <stloyd@gmail.com>
2011-12-05 13:43:07 +0100

    Failing test fix #1

--- src/Knp/Bundle/KnpBundlesBundle/Tests/Git/RepoTest.php
+++ src/Knp/Bundle/KnpBundlesBundle/Tests/Git/RepoTest.php
@@ -13,7 +13,7 @@ class RepoTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($repo->getGitRepo() instanceof \PHPGit_Repository);
     }
 
-    protected function getRepo($repoFullName = 'knplabs/KnpGaufretteBundle')
+    protected function getRepo($repoFullName = 'KnpLabs/KnpGaufretteBundle')
     {
         $dir = sys_get_temp_dir().'/kb_git_repos';
         $manager = new RepoManager($dir, $_SERVER['GIT_BIN']);
