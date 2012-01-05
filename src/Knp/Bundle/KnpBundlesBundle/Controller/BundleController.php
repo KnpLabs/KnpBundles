01619b5ab87645b9c6d19c3564ef8738774b32e0
Joseph Bielawski <stloyd@gmail.com>
2012-01-05 10:39:34 +0100

    Fixed some minor issues

--- src/Knp/Bundle/KnpBundlesBundle/Controller/BundleController.php
+++ src/Knp/Bundle/KnpBundlesBundle/Controller/BundleController.php
@@ -248,7 +248,7 @@ class BundleController extends BaseController
         }
 
         // Save only if sender is owner of bundle
-        if ($bundle->getUser() === $this->getUser()) {
+        if ($bundle->getUser() == $this->get('security.context')->getToken()->getUser()) {
             $state = $this->getRequest()->request->get('state', 'unknown');
 
             $bundle->setState($state);
