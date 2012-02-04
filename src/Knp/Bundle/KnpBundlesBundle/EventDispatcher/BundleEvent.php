b97c9cd8dc0072f6d06eec29da970aea61ea693c
Romain Pouclet <me@palleas.com>
2012-02-04 07:32:41 +0100

    Add a dedicated event class for bundle-related events

---
+++ src/Knp/Bundle/KnpBundlesBundle/EventDispatcher/BundleEvent.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Knp\Bundle\KnpBundlesBundle\EventDispatcher;
+
+use Knp\Bundle\KnpBundlesBundle\Entity\Bundle;
+
+/**
+* 
+*/
+class BundleEvent extends Event
+{
+    
+    const UPDATE_SCORE = 'bundle.update_score';
+
+    /**
+     * @var Knp\Bundle\KnpBundlesBundle\Entity\Bundle
+     */
+    private $bundle;
+
+    /**
+     * @param Knp\Bundle\KnpBundlesBundle\Entity\Bundle
+     */
+    public function __construct(Bundle $bundle)
+    {
+        $this->bundle = $bundle;
+    }
+
+    /**
+     * @return Knp\Bundle\KnpBundlesBundle\Entity\Bundle
+     */
+    public function getBundle()
+    {
+        return $this->bundle;
+    }
+}
