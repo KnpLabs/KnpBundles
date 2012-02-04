348ba946e363f2c1c904677fb3858af8fec86b45
Romain Pouclet <me@palleas.com>
2012-02-04 08:05:03 +0100

    Add the very first scoring factor : readme.

--- src/Knp/Bundle/KnpBundlesBundle/Entity/Bundle.php
+++ src/Knp/Bundle/KnpBundlesBundle/Entity/Bundle.php
@@ -111,6 +111,12 @@ class Bundle
     protected $scores = null;
 
     /**
+     *
+     * @var array
+     */
+    protected $scoreDetails;
+
+    /**
      * Repo creation date (on this website)
      *
      * @ORM\Column(type="datetime")
@@ -234,6 +240,7 @@ class Bundle
         $this->createdAt = new \DateTime('NOW');
         $this->updatedAt = new \DateTime('NOW');
         $this->score = 0;
+        $this->scoreDetails = array();
         $this->scores = new ArrayCollection();
         $this->lastCommitAt = new \DateTime('2010-01-01');
         $this->lastCommits = serialize(array());
@@ -460,6 +467,11 @@ class Bundle
         return $this->scores;
     }
 
+    public function addScoreDetail($name, $value)
+    {
+        $this->scoreDetails[$name] = $value;
+    }
+
     /**
      * Returns details about the bundle's score
      */
@@ -501,7 +513,7 @@ class Bundle
      */
     public function recalculateScore()
     {
-        $score = array_sum($this->getScoreDetails());
+        $score = array_sum($this->scoreDetails);
 
         $this->setScore($score);
     }
