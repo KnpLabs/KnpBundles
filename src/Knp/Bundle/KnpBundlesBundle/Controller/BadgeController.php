a82840a6ae04ef8972b3b677f71ac63db1158b10
Alexandr Torchenko <torchenko@gmail.com>
2012-02-10 19:22:53 +0200

    Comments processed

--- src/Knp/Bundle/KnpBundlesBundle/Controller/BadgeController.php
+++ src/Knp/Bundle/KnpBundlesBundle/Controller/BadgeController.php
@@ -26,17 +26,53 @@ class BadgeController extends BaseController
             throw new NotFoundHttpException(sprintf('The badge is missing for "%s/%s"', $username, $name));
         }
 
+        $relativePath = $this->findShortestPath(
+            $this->container->getParameter('kernel.root_dir'),
+            $this->container->getParameter('kernel.cache_dir')
+        );
+
         return $this->get('igorw_file_serve.response_factory')->create(
-            $this->getRelativePath().'/badges/'.$username.'-'.$name.'.png',
+            $relativePath.'/badges/'.$username.'-'.$name.'.png',
             'image/png'
         );
     }
 
-   private function getRelativePath() {
-        return str_replace(
-            $this->container->getParameter('kernel.root_dir').'/',
-            '',
-            $this->container->getParameter('kernel.cache_dir')
-        );
+    private function findShortestPath($from, $to)
+    {
+        if (!$this->isAbsolutePath($from) || !$this->isAbsolutePath($to)) {
+            throw new \InvalidArgumentException('from and to must be absolute paths');
+        }
+
+        if (dirname($from) === dirname($to)) {
+            return './'.basename($to);
+        }
+        $from = lcfirst(rtrim(strtr($from, '\\', '/'), '/'));
+        $to = lcfirst(rtrim(strtr($to, '\\', '/'), '/'));
+
+        $commonPath = $to;
+        while (strpos($from, $commonPath) !== 0 && '/' !== $commonPath && !preg_match('{^[a-z]:/?$}i', $commonPath) && '.' !== $commonPath) {
+            $commonPath = strtr(dirname($commonPath), '\\', '/');
+        }
+
+        if (0 !== strpos($from, $commonPath) || '/' === $commonPath || '.' === $commonPath) {
+            return $to;
+        }
+
+        $commonPath = rtrim($commonPath, '/') . '/';
+        $sourcePathDepth = substr_count(substr($from, strlen($commonPath)), '/');
+        $commonPathCode = str_repeat('../', $sourcePathDepth);
+
+        return ($commonPathCode . substr($to, strlen($commonPath))) ?: './';
+    }
+
+    /**
+     * Checks if the given path is absolute
+     *
+     * @param string $path
+     * @return Boolean
+     */
+    private function isAbsolutePath($path)
+    {
+        return substr($path, 0, 1) === '/' || substr($path, 1, 1) === ':';
     }
 }
