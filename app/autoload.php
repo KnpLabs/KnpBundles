7830a39ea562e9fd9becd56713d4546b1921ec1b
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-02-12 12:28:07 +0100

    add NelmioSolariumBundle and Solarium lib, initialize kernel and configuration.

--- app/autoload.php
+++ app/autoload.php
@@ -35,13 +35,15 @@ $loader->registerNamespaces(array(
     'PhpAmqpLib'                    => __DIR__.'/../vendor/videlalvaro/php-amqplib',
     'Imagine'                       => __DIR__.'/../vendor/imagine/lib',
     'Igorw'                         => __DIR__.'/../vendor/bundles',
+    'Nelmio'                        => __DIR__.'/../vendor/bundles',
 ));
 
 $loader->registerPrefixes(array(
     'Twig_Extensions_'              => __DIR__.'/../vendor/twig-extensions/lib',
     'Twig_'                         => __DIR__.'/../vendor/twig/lib',
     'Github_'                       => __DIR__.'/../vendor/php-github-api/lib',
-    'PHPGit_'                       => __DIR__.'/../vendor/php-git-repo/lib'
+    'PHPGit_'                       => __DIR__.'/../vendor/php-git-repo/lib',
+    'Solarium_'                     => __DIR__.'/../vendor/solarium/library',
 ));
 $loader->register();
 
