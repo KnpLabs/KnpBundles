246721237d84a548c152dcae6415f39bb8091c4e
Joseph Bielawski <stloyd@gmail.com>
2012-03-27 14:13:33 +0200

    Introduce usage of composer! (also fixed failed tests)

--- web/app_dev.php
+++ web/app_dev.php
@@ -6,10 +6,14 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
     die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../app/bootstrap.php';
+require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
-$kernel->handle(Request::createFromGlobals())->send();
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
