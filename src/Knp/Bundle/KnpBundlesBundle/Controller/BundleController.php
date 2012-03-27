47b59515231bd9b3f89b85b9ba8d7db296312145
Romain Pouclet <me@palleas.com>
2012-03-27 16:19:58 +0200

    Update recommenders count

--- src/Knp/Bundle/KnpBundlesBundle/Controller/BundleController.php
+++ src/Knp/Bundle/KnpBundlesBundle/Controller/BundleController.php
@@ -199,7 +199,7 @@ class BundleController extends BaseController
         if ($user->isUsingBundle($bundle)) {
             $bundle->updateScore(-5);
 
-            $bundle->getRecommenders()->removeElement($user);
+            $bundle->removeRecommender($user);
             $user->getUsedBundles()->removeElement($bundle);
         } else {
             $bundle->updateScore(5);
