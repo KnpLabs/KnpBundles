1e3b1a9881889b641adb6181769be5c2a56874f5
Luis Cordova <cordoval@gmail.com>
2012-06-19 22:04:02 -0500

    first attempt to implement test for remove command

--- src/Knp/Bundle/KnpBundlesBundle/Tests/Commands/KbRemoveWrongBundlesCommandTest.php
+++ src/Knp/Bundle/KnpBundlesBundle/Tests/Commands/KbRemoveWrongBundlesCommandTest.php
@@ -13,6 +13,8 @@ class KbRemoveWrongBundlesCommandTest extends \PHPUnit_Framework_TestCase
 {
     public function testRemovingOneInvalidBundleFromDatabase()
     {
+        $this->markAsIncomplete('rather trying to test updater service');
+
         $kernel = new \AppKernel('test', true);
         $kernel->boot();
 
