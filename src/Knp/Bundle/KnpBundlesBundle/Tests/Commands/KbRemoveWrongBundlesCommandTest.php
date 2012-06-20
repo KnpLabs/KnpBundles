1d27acee654bb4f2913f35141a05ae19b27b8453
Luis Cordova <cordoval@gmail.com>
2012-06-19 20:39:21 -0500

    cleaned up wip

--- src/Knp/Bundle/KnpBundlesBundle/Tests/Commands/KbRemoveWrongBundlesCommandTest.php
+++ src/Knp/Bundle/KnpBundlesBundle/Tests/Commands/KbRemoveWrongBundlesCommandTest.php
@@ -11,19 +11,7 @@ use Knp\Bundle\KnpBundlesBundle\Command\KbRemoveWrongBundlesCommand;
 
 class KbRemoveWrongBundlesCommandTest extends \PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
-        if (!function_exists('gd_info')) {
-            $this->markTestSkipped();
-        }
-        $info = gd_info();
-
-        if (!$info['FreeType Support']) {
-            $this->markTestSkipped();
-        }
-    }
-
-    public function testBadgeGenerator()
+    public function testRemovingOneInvalidBundleFromDatabase()
     {
         $kernel = new \AppKernel('test', true);
         $kernel->boot();
@@ -31,18 +19,10 @@ class KbRemoveWrongBundlesCommandTest extends \PHPUnit_Framework_TestCase
         $application = new Application($kernel);
         $application->add(new KbRemoveWrongBundlesCommand());
 
-        try {
-            $command = $application->find('kb:remove:wrong-bundles');
-            $commandTester = new CommandTester($command);
-            $commandTester->execute(array('command' => $command->getName()));
+        $command = $application->find('kb:remove:wrong-bundles');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
 
-            $this->assertRegExp('/generated/', $commandTester->getDisplay());
-        } catch (RuntimeException $e) {
-            if ('GD is not compiled with FreeType support' == $e->getMessage()) {
-                $this->markTestSkipped('GD is not compiled with FreeType support');
-            } else {
-                throw $e;
-            }
-        }
+        $this->assertRegExp('/generated/', $commandTester->getDisplay());
     }
 }
