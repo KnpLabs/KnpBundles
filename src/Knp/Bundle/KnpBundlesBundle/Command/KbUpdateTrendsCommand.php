bc9fd43af16b0108f3d79b1b6d4ef2be4a975a95
Evgeniy Guseletov <seo-in@ya.ru>
2012-08-21 08:31:57 +0000

    Sql parameters

--- src/Knp/Bundle/KnpBundlesBundle/Command/KbUpdateTrendsCommand.php
+++ src/Knp/Bundle/KnpBundlesBundle/Command/KbUpdateTrendsCommand.php
@@ -53,7 +53,7 @@ class KbUpdateTrendsCommand extends ContainerAwareCommand
         $q = $em->createQuery('UPDATE Knp\Bundle\KnpBundlesBundle\Entity\Bundle bundle SET bundle.trend1 = 0');
         $q->execute();
 
-        $sql = <<<EOF
+        $query = <<<EOF
 UPDATE bundle
 
 JOIN (
@@ -70,17 +70,15 @@ JOIN (
     WHERE date = CURRENT_DATE
 ) score
   ON score.bundle_id = bundle.id
-  AND score.diff > %s
+  AND score.diff > :minDiff
 
 SET trend1 = score.diff
-WHERE score >= %s
+WHERE score >= :minThreshold
 EOF;
 
         $minDiff = $this->getContainer()->getParameter('knp_bundles.trending_bundle.min_score_diff');
         $minThreshold = $this->getContainer()->getParameter('knp_bundles.trending_bundle.min_score_threshold');
-        $query = sprintf($sql, $minDiff, $minThreshold);
-
-        $nbRows = $em->getConnection()->executeUpdate($query);
+        $nbRows = $em->getConnection()->executeUpdate($query, array('minDiff' => $minDiff, 'minThreshold' => $minThreshold));
 
         return $nbRows;
     }
