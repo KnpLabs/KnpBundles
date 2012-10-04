91e16a26379b1d0b8d029aadd26544ead13679ac
Evgeniy Guseletov <seo-in@ya.ru>
2012-10-04 15:47:36 +0000

    Add scores only for updated bundles

--- src/Knp/Bundle/KnpBundlesBundle/Tests/Entity/BundleTest.php
+++ src/Knp/Bundle/KnpBundlesBundle/Tests/Entity/BundleTest.php
@@ -3,6 +3,7 @@
 namespace Knp\Bundle\KnpBundlesBundle\Tests\Git;
 
 use Knp\Bundle\KnpBundlesBundle\Entity\Bundle;
+use Knp\Bundle\KnpBundlesBundle\Entity\Score;
 
 class BundleTest extends \PHPUnit_Framework_TestCase
 {
@@ -24,5 +25,50 @@ class BundleTest extends \PHPUnit_Framework_TestCase
         $bundle = new Bundle('knplabs/KnpMenuBundle');
         $bundle->setNbForks(1);
         $this->assertTrue($bundle->isInitialized());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldHaveChangesWithSameScore()
+    {
+        $bundle = new Bundle('knplabs/KnpMenuBundle');
+        $bundle->setScore(1000);
+        $bundle->setReadme('readme number one');
+        $bundle->setLastCommitAt(new \DateTime('-10 day'));
+        $bundle->setNbFollowers(100);
+        $bundle->setNbForks(10);
+
+        $beforeChange = $bundle->getStatusHash();
+
+        $bundle->setReadme('readme number two');
+        $bundle->setLastCommitAt(new \DateTime());
+        $bundle->setNbFollowers(10);
+        $bundle->setNbForks(100);
+
+        $afterChange = $bundle->getStatusHash();
+
+        $this->assertNotEquals($beforeChange, $afterChange);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotHaveChangesWithOnlyChangedScore()
+    {
+        $bundle = new Bundle('knplabs/KnpMenuBundle');
+        $bundle->setScore(1000);
+        $bundle->setReadme('readme number one');
+        $bundle->setLastCommitAt(new \DateTime('-10 day'));
+        $bundle->setNbFollowers(100);
+        $bundle->setNbForks(10);
+
+        $beforeChange = $bundle->getStatusHash();
+
+        $bundle->setScore(1100);
+
+        $afterChange = $bundle->getStatusHash();
+
+        $this->assertEquals($beforeChange, $afterChange);
     }
 }
