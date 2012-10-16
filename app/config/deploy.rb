ee0c8966b52d3699a598727f162f3978b74e7854
Luis Cordova <cordoval@gmail.com>
2012-10-15 20:18:53 -0500

    initial info in for configuration still monostage

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,11 +1,21 @@
-set :application, "set your application name here"
+set :application, "KnpBundles"
 set :domain,      "#{application}.com"
 set :deploy_to,   "/var/www/#{domain}"
 set :app_path,    "app"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
 set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set :scm_command, "/usr/bin/git"
+set :scm_verbose, true
+set :repository,  "git@github.com:KnpLabs/#{application}.git"
+
+set :branch, "feature/263"
+set :user, "cordoval"
+set :password, "cordoval"
+
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", app_path + "/cache", "vendor"]
+set :update_vendors, true
+set :dump_assetic_assets, true
 
 set :model_manager, "doctrine"
 # Or: `propel`
@@ -14,7 +24,9 @@ role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
+set  :use_sudo, false
 set  :keep_releases,  3
+ssh_options[:forward_agent] = true
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
