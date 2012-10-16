10b03e5f5e450b8b444b3409e41a0773ceee0830
Luis Cordova <cordoval@gmail.com>
2012-10-15 21:14:31 -0500

    switch to multistage

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,32 +1,46 @@
-set :application, "KnpBundles"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
+set :stages,        %w(prod vps100)
+set :default_stage, "vps100"
+set :stage_dir,     "app/config/deploy"
+require 'capistrano/ext/multistage'
+
+set :application, "KnpBundles.com"
 set :app_path,    "app"
+set :group_writable, false
+set :use_sudo, false
 
+ssh_options[:port] = "22123"
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+set :repository,  "git@github.com:KnpLabs/KnpBundles.git"
 set :scm,         :git
-set :scm_command, "/usr/bin/git"
-set :scm_verbose, true
-set :repository,  "git@github.com:KnpLabs/#{application}.git"
-
-set :branch, "feature/263"
-set :user, "cordoval"
-set :password, "cordoval"
-
-set :shared_files, ["app/config/parameters.yml"]
-set :shared_children, [app_path + "/logs", app_path + "/cache", "vendor"]
-set :update_vendors, true
-set :dump_assetic_assets, true
+set :deploy_via, :remote_cache
 
 set :model_manager, "doctrine"
-# Or: `propel`
+set :admin_runner, nil
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain                         # This may be the same as your `Web` server
-role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
-
-set  :use_sudo, false
 set  :keep_releases,  3
-ssh_options[:forward_agent] = true
-
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
+
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", app_path + "/sessions"]
+set :use_composer, true
+set :update_vendors, true
+set :vendors_mode, "install"
+set :dump_assetic_assets, true
+set :interactive_mode, false
+set :use_sudo, false
+
+before 'symfony:composer:update', 'symfony:copy_vendors'
+before 'symfony:cache:warmup', 'symfony:force_migrate'
+
+namespace :symfony do
+  desc "Copy vendors from previous release"
+  task :copy_vendors, :except => { :no_release => true } do
+    pretty_print "--> Copying vendors from previous release if exists"
+    run "if [ -d #{previous_release}/vendor ]; then cp -a #{previous_release}/vendor #{latest_release}/vendor; fi"
+    puts_ok
+  end
+end
