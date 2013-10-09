8ea31dda1ab7c9b9c0477a034cc69820cd74d650
Nek (Maxime Veber) <maxime.veber@knplabs.com>
2013-10-09 11:12:30 +0200

    Removed useless historical code

--- src/Knp/Bundle/KnpBundlesBundle/Finder/Github.php
+++ src/Knp/Bundle/KnpBundlesBundle/Finder/Github.php
@@ -11,58 +11,6 @@ use Github\Client;
  */
 class Github implements FinderInterface
 {
-    const ENDPOINT         = 'https://github.com/search';
-    const PARAMETER_QUERY  = 'q';
-    const PARAMETER_START  = 'start_value';
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function buildUrl($page)
-    {
-        $params = array(
-            self::PARAMETER_QUERY => $this->query,
-            'repo'                => null,
-            'langOverride'        => null,
-            'type'                => 'Repositories',
-            'language'            => 'PHP',
-        );
-
-        if ($page > 1) {
-            $params[self::PARAMETER_START] = $page;
-        }
-
-        return self::ENDPOINT . '?' . http_build_query($params);
-    }
-
-    /**
-     * Extracts the urls from the given google results crawler
-     *
-     * @param  Crawler $crawler
-     *
-     * @return array
-     */
-    protected function extractPageUrls(Crawler $crawler)
-    {
-        return $crawler->filter('#container h3.repolist-name a')->extract('href');
-    }
-
-    /**
-     * Returns the github repository extracted from the given URL
-     *
-     * @param  string $url
-     *
-     * @return string or NULL if the URL does not contain any repository
-     */
-    protected function extractUrlRepository($url)
-    {
-        if (preg_match('/https:\/\/github\.com\/(?<username>[\w\.-]+)\/(?<repository>[\w\.-]+)/', $url, $matches)) {
-            return $matches['username'] . '/' . $matches['repository'];
-        }
-
-        return null;
-    }
-
     /**
      * @var string
      */
@@ -110,5 +58,21 @@ class Github implements FinderInterface
         }
 
         return $repositories;
+    }
+
+    /**
+     * Returns the github repository extracted from the given URL
+     *
+     * @param  string $url
+     *
+     * @return string or NULL if the URL does not contain any repository
+     */
+    protected function extractUrlRepository($url)
+    {
+        if (preg_match('/https:\/\/github\.com\/(?<username>[\w\.-]+)\/(?<repository>[\w\.-]+)/', $url, $matches)) {
+            return $matches['username'] . '/' . $matches['repository'];
+        }
+
+        return null;
     }
 }
