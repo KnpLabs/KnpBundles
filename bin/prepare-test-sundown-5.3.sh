788ba131d012dc585b7fc7360835b7a48944e747
Maxime Veber (aka Nek) <nek.dev@gmail.com>
2014-06-02 14:11:34 +0200

    Fixed 5.3 test

--- bin/prepare-test-sundown-5.3.sh
+++ bin/prepare-test-sundown-5.3.sh
@@ -1,3 +1,3 @@
-pecl -q install sundown-beta && sh -c "echo 'extension=sundown.so' >> `php --ini | grep 'Loaded Configuration' | sed -e 's|.*:\s*||'`"
+pecl -q install sundown-beta
 # in linux you may want to execute this as below
 # sudo pecl -q install sundown && sudo sh -c "echo 'extension=sundown.so' >> `php --ini | grep 'Loaded Configuration' | sed -e 's|.*:\s*||'`"
