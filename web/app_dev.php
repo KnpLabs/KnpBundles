502f0237ee2797c181d89d8222bc8d4222591b3c
Lénaïc <lcouellan@gmail.com>
2018-09-07 10:38:20 +0200

    Update security in app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,6 +2,9 @@
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
+    die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
